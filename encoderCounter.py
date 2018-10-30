bda31c9470715d7657e03dcf88d2a999066efab6
benjamin standfield <benjaminstandfield@gmail.com>
2018-10-30 16:58:21 -0400

    testing forward backward

--- encoderCounter.py
+++ encoderCounter.py
@@ -11,18 +11,26 @@ GPIO.setup(dt2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 counter2 = 0
 clkLastState2 = GPIO.input(clk2)
 
+direction = ""
+
 try:
-        while True:
-                clkState2 = GPIO.input(clk2)
-                dtState2 = GPIO.input(dt2)
+    while True:
+        clkState2 = GPIO.input(clk2)
+        dtState2 = GPIO.input(dt2)
+        if dtState2 and direction != "forward"
+            direction="forward"
+            print "forward"
+        if clkState2 and direction != "backward"
+            direction="backward"
+            print "backward"
 		print str(clkState2) + " " + str(dtState2)
-                if clkState2 != clkLastState2:
-                        if dtState2 != clkState2:
-                                counter2 += 1
-                        else:
-                                counter2 -= 1
-                clkLastState2 = clkState2
-                sleep(0.01)
+        if clkState2 != clkLastState2:
+            if dtState2 != clkState2:
+                counter2 += 1
+            else:
+                counter2 -= 1
+        clkLastState2 = clkState2
+        sleep(0.01)
 		print counter2
 finally:
         GPIO.cleanup()
