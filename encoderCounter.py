ba207c237fb771dfc95ce9fa24f627a3479a47e4
benjamin <benjaminstandfield@gmail.com>
2018-10-30 21:07:12 +0000

    bug fixes

--- encoderCounter.py
+++ encoderCounter.py
@@ -17,20 +17,20 @@ try:
     while True:
         clkState2 = GPIO.input(clk2)
         dtState2 = GPIO.input(dt2)
-        if dtState2 and direction != "forward"
+        if dtState2:
             direction="forward"
             print "forward"
-        if clkState2 and direction != "backward"
+        if clkState2:
             direction="backward"
             print "backward"
-		print str(clkState2) + " " + str(dtState2)
+	#print str(clkState2) + " " + str(dtState2)
         if clkState2 != clkLastState2:
             if dtState2 != clkState2:
                 counter2 += 1
             else:
                 counter2 -= 1
         clkLastState2 = clkState2
-        sleep(0.01)
-		print counter2
+        sleep(0.1)
+	#print counter2
 finally:
         GPIO.cleanup()
