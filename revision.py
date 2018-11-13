d70cfdbe96d126519a0b033d1c967876f8aeebbe
benjamin standfield <benjaminstandfield@gmail.com>
2018-11-13 17:41:03 -0500

    increased sampling rate, added counter for pulses.

--- revision.py
+++ revision.py
@@ -8,8 +8,9 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-counter = 0
-counter1 = 0
+forwardCcounter = 0
+backwardCounter = 0
+pulseCounter = 0;
 clkLastState = GPIO.input(clk)
 
 direction = ""
@@ -18,28 +19,26 @@ try:
     while True:
         clkState = GPIO.input(clk)
         dtState = GPIO.input(dt)
-        if clkState != clkLastState:
-	    print "diff"
-            if clkState:
-		print "up"
-                if dtState:
-			if direction != "forward":
-                    		direction = "forward"
-				counter = counter+1
-                    		print direction + " " + str(counter)
-			else:
-				counter = counter+1
-				print direction + " " + str(counter)
+        if clkState && (clkState != clkLastState):
+            pulseCounter = pulseCounter + 1
+            if dtState:
+                if direction != "forward":
+                    direction = "forward"
+                    forwardCounter = forwardCounter+1
+                    print direction + " " + str(forwardCounter)
                 else:
-			if direction != "backward":
-				counter1 = counter1+1
-                		direction = "backward"
-                		print direction + " " + str(counter1)
-			else:
-				counter1 = counter1+1
-				print direction + " " + str(counter)
+                    counter = counter+1
+                    print direction + " " + str(counter)
+            else:
+                if direction != "backward":
+                    counter1 = counter1+1
+                    direction = "backward"
+                    print direction + " " + str(counter1)
+                else:
+                    counter1 = counter1+1
+                    print direction + " " + str(counter)
         clkLastState = clkState
-        sleep(0.01)
-	#print counter
+        sleep(0.001)
+#print counter
 finally:
-        GPIO.cleanup()
+GPIO.cleanup()
