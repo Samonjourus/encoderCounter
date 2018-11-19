977744e93dbb1dc7e7e3769564bf2983c512ee81
benjamin <benjaminstandfield@gmail.com>
2018-11-19 18:43:20 +0000

    wang did shit

--- revision.py
+++ revision.py
@@ -5,17 +5,24 @@ clk = 26
 dt = 20
 
 global ecount
+global ecount2
 ecount =0
+ecount2 =0
 
 def my_callback(i):
 	global ecount
 	ecount = ecount+1
-	print str(ecount)
+	print 'x '+str(ecount)
+def my_callback2(i):
+	global ecount2
+	ecount2 = ecount2+1
+	print 'y '+str(ecount2)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(26, GPIO.RISING, callback=my_callback, bouncetime=1)
+GPIO.add_event_detect(26, GPIO.RISING, callback=my_callback)
+GPIO.add_event_detect(20, GPIO.RISING, callback=my_callback2)
 
 forwardCounter = 0
 backwardCounter = 0
@@ -27,6 +34,7 @@ direction = ""
 try:
     while True:
         clkState = GPIO.input(clk)
+#	print str(clkState)
         dtState = GPIO.input(dt)
         if clkState and (clkState != clkLastState):
             pulseCounter = pulseCounter + 1
@@ -48,7 +56,7 @@ try:
                     backwardCounter = backwardCounter+1
                     print direction + " pulses: " + str(backwardCounter)
         clkLastState = clkState
-        #sleep(0.001)
+        sleep(0.01)
 #print counter
 finally:
 	GPIO.cleanup()
